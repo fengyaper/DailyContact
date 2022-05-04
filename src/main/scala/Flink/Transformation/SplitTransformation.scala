38a9b209a91d9bebabbbf70590a6b7c136264783
Administrator <1021423806@qq.com>
2022-05-04 16:27:10 +0800

    wordcount UI 使用

---
+++ src/main/scala/Flink/Transformation/SplitTransformation.scala
@@ -0,0 +1,23 @@
+package Flink.Transformation
+
+import org.apache.flink.streaming.api.scala.{DataStream, SplitStream, StreamExecutionEnvironment}
+
+object SplitTransformation {
+  def main(args: Array[String]): Unit = {
+//    split +select算子联合使用可以达到分流的作用
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+    val dStream: DataStream[Long] = env.generateSequence(1, 100)
+    val value: SplitStream[Long] = dStream.split(d => {
+      d % 2 match {
+        case 0 => List("f")
+        case 1 => List("s")
+      }
+    })
+    val value1: DataStream[Long] = value.select("f")
+
+    value1.print()
+    env.execute()
+
+  }
+
+}
